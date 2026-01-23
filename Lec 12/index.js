c5707944bb0207020b7d8b01aff48d91fc05aa65
rajpatle799 <rajpatle799@gmail.com>
2026-01-23 23:38:52 +0530

    new

---
+++ Lec 12/index.js
@@ -0,0 +1,10 @@
+console.log("Hello");
+
+let btn=document.querySelector("button")
+let body=document.querySelector("body")
+
+btn.addEventListener("click",function(){
+    console.log("hello");
+    body.style.background="blue"
+}
+)
